cf1ddb4e1817a595311849aca440bb86ebdce56f
geisabitt <ageisislane@gmail.com>
2023-12-13 23:23:11 -0300

    refactor funcionamento

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ import Accordion from './modules/accordion.js'
 import DropdownMenu from './modules/dropdown-menu.js'
 import fetchAnimais from './modules/fetch-animais.js'
 import fetchBitcoin from './modules/fetch-bitcoin.js'
-import initFuncionamento from './modules/funcionamento.js'
 import MenuMobile from './modules/menu-mobile.js'
 import Modal from './modules/modal.js'
 import ScrollAnima from './modules/scroll-anima.js'
@@ -34,7 +33,9 @@ dropdownMenu.init()
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]')
 menuMobile.init()
 
-initFuncionamento()
+const funcionamento = new Functionamento('[data-semana]', 'aberto')
+funcionamento.init()
+
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco')
 
 fetchAnimais('../../animais.json', '.numeros-grid')
